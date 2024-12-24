bf30d5bd9e039de224aa7b9e58a27116a2db6e3a
Kingbee1M <160544114+Kingbee1M@users.noreply.github.com>
2024-12-25 00:35:38 +0100

    Update page.js

--- src/app/ClientDashboard/page.js
+++ src/app/ClientDashboard/page.js
@@ -11,7 +11,7 @@ const OpenSans = Open_Sans ({
     subsets: ["greek", "latin"]
 })
 export default function ClientDashboard () {
-    const userData = useUserStore((state) => state.apiResponse)
+    const userData = useUserStore((state) => state.apiResponse?.[1]?.data?.user || null)
     const userOrder = useUserStore((state) => state.apiResponse[0])
     console.log(userData);
     
